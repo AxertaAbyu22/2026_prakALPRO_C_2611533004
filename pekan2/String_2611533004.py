02a903ebbccdfb5f9f513ec5da66cfca445e0d49
zaky747 <zakyafiqtaha@gmail.com>
2026-09-10 11:50:00 +0700

    pekan 2

---
+++ pekan2/String_2611533004.py
@@ -0,0 +1,12 @@
+nama_3004 = "Abiyyu"
+jenis_kelamin_3004= 'L'
+alamat_3004="""
+    jalan belibis blok a no 5
+    Kec.padang utara
+    Kota Padang
+"""
+agama_3004 = 'islam'
+print("Nama saya adalah: ", nama_3004)
+print(" Jenis kelamin saya adalah:", jenis_kelamin_3004)
+print(" Alamat saya adalah:", alamat_3004)
+print(" Agama saya adalah:", agama_3004)
