2131d4d11bddce4c89725b7ed75dc024090ade68
unknown <muhammadabiyyu221@gmail.com>
2026-09-12 23:29:04 +0700

    Upload tugas tipe data dan variable python

---
+++ Laprak alpro 2/Input_2611533004.py
@@ -0,0 +1,10 @@
+# Buat file dengan nama Input_NIM.py
+# Buat program untuk menghitung luas persegi panjang
+# Nama variable ditambah 4 digit terakhir NIM contoh:panjang_3004, lebar_3004, luas_3004
+# Program menghitung luas persegi panjang
+# memasukkan nilai panjang dan lebar persegi panjang
+# Nilai yang dimasukkan akan di konversikan oleh output
+panjang_3004=input("masukkan panjang: ")
+lebar_3004=input("masukkan lebar: ")
+luas_3004= int(panjang_3004) * int(lebar_3004)
+print("luas persegi panjang= ", luas_3004)
---
+++ Laprak alpro 2/Numerik_2611533004.py
@@ -0,0 +1,14 @@
+# Buat file dengan nama Numerik_NIM.py
+# nama variable ditambah 4 digit terakhir NIM contoh:a_3004
+#create a variable with numeric type value.
+
+a_3004=100
+print("The type of variable having value", a_3004, "is", type(a_3004))
+
+#create a variable with float type value.
+b_3004=10.2345
+print("The type of variable having value", b_3004, "is", type (b_3004))
+
+#create a variable with complex type value.
+c_3004=100+3j
+print("The type of variable having value", c_3004, "is", type (c_3004))
